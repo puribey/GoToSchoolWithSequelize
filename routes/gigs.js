3ca3bfef8f6214ee9f0e4e59464fc7c35f4a4e2c
puribey <kenshipichu@gmail.com>
2019-09-09 10:02:57 -0300

    feat: search bar working

--- routes/gigs.js
+++ routes/gigs.js
@@ -2,6 +2,8 @@ const express = require("express");
 const router = express.Router();
 const db = require("../config/database");
 const Gig = require("../models/Gig");
+const Sequelize = require("sequelize");
+const Op = Sequelize.Op;
 
 // Get gig list
 router.get("/", (req, res) =>
@@ -53,5 +55,24 @@ router.post("/add", (req, res) => {
       .catch(err => console.log(err));
   }
 });
+
+// Search for gigs
+router.get("/search", (req, res) => {
+  let { term } = req.query;
+  // Make lowercase
+  term = term.toLowerCase();
+
+  Gig.findAll({
+    where: {
+      technologies: {
+        [Op.like]: "%" + term + "%"
+      }
+    }
+  })
+    .then(gigs => res.render("gigs", { gigs }))
+    .catch(err => console.log(err));
+});
+
+// Delete 
 
 module.exports = router;
